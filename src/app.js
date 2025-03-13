0489f94223f33e660c96eec123a7619d2745a1a4
MHS676 <Cool@676>
2025-03-13 15:43:06 +0600

    final project

---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const connectDB = require("./config/db");
+
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+
+
+const categoryRoutes = require("./routes/categoryRoutes");
+const productRoutes = require("./routes/productRoutes");
+
+app.use("/categories", categoryRoutes);
+app.use("/products", productRoutes);
+
+
+connectDB();
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(` Server running on http://localhost:${PORT}`);
+});
+
+
+module.exports = app;
